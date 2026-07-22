f4132acbd0606fa602cf960733dbfad6c0956654
ruhan2000 <ruhan.behlim@aminsofttech.co>
2026-07-22 17:47:47 +0530

    new commite

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,12 @@
-const admin = require("firebase-admin");
 const serviceAccount = require("../firebase-service-account.json");
+
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
